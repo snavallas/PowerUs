7776452b40d29445f3e270c78897fe81928cf1ec
Santiago J Navallas <snavallas@gmail.com>
2024-02-25 15:25:59 +0100

    First happy path

--- cypress/e2e/arbeitgeber-flow/consultation-form.cy.ts
+++ cypress/e2e/arbeitgeber-flow/consultation-form.cy.ts
@@ -1,6 +1,6 @@
 describe('Arbeitgeber', () => {
   it('can submit a free consultation', () => {
-    cy.intercept({ hostname: 'api.testing.powerus.de' }).as('getActivities')
+    // cy.intercept({ hostname: 'api.testing.powerus.de' }).as('getActivities')
 
     cy.viewport('iphone-8')
     cy.visit('https://testing.powerus.de/')
@@ -31,7 +31,8 @@ describe('Arbeitgeber', () => {
     boxAnlagenmechanik.click()
 
     // Click Weiter button
-    const buttonWeiter = cy.get('.mat-button-wrapper').contains('Weiter')
+    // const buttonWeiter = cy.get('.mat-button-wrapper').contains('Weiter')
+    const buttonWeiter = cy.get('app-funnel-steps').find('.mat-button-wrapper').contains('Weiter')
     buttonWeiter.click({ force: true })
 
     // Select Number of positions
@@ -39,9 +40,46 @@ describe('Arbeitgeber', () => {
     chipThreeFive.click()
 
     // Enter City
-    // const textBoxCity = cy.get('input[id="mat-input-8"]')
-    // const textBoxCity = cy.get(`[id='mat-input-8']`)
-    const textBoxCity = cy.get('.mat-input-8')
+    const textBoxCity = cy.get('input[id="mat-input-1"]')
     textBoxCity.type('Berlin', { force: true })
+
+    // Click Weiter button
+    // const buttonWeiter2 = cy.get('.mat-button-wrapper').contains('Weiter')
+    const buttonWeiter2 = cy.get('app-hiring-location-step').find('.mat-button-wrapper').contains('Weiter')
+    buttonWeiter2.click({ force: true })
+
+    // Enter Unternehmen
+    const textBoxUnternehmen = cy.get('input[id="mat-input-2"]')
+    textBoxUnternehmen.type('PowerUs', { force: true })
+
+    // Click Zum letzten Schritt button
+    const buttonNext = cy.get('.mat-button-wrapper').contains('Zum letzten Schritt')
+    buttonNext.click({ force: true })
+
+    // Enter Contact information
+    const textBoxFirstName = cy.get('input[id="mat-input-3"]')
+    textBoxFirstName.type('Max', { force: true })
+    const textBoxLastName = cy.get('input[id="mat-input-4"]')
+    textBoxLastName.type('Mustermann', { force: true })
+    const textBoxEmail = cy.get('input[id="mat-input-5"]')
+    textBoxEmail.type('max.mustermann@powerus.com', { force: true })
+    const textBoxPhone = cy.get('input[id="mat-input-6"]')
+    textBoxPhone.type('15143211257', { force: true })
+    textBoxPhone.blur()
+
+    // Wait until email and phone number are validated
+    cy.intercept('GET', '**//validate/phone-number**').as('validatePhoneNumber')
+    // cy.wait('@validatePhoneNumber').its('response.statusCode').should('eq', 200)
+    cy.wait('@validatePhoneNumber').then((intercept)=>{
+      expect(intercept.response.body.isValid).to.be.true; //should work
+  })
+      // .should('have.property', '.isValid', true)
+
+    // Click Kostenlos beraten lassen button
+    const buttonSubmit = cy.get('app-contact-information-step').find('.mat-button-wrapper').contains('Kostenlos')
+    buttonSubmit.click({ force: true })
+
+    const firstTitle = cy.get('h1').first()
+    firstTitle.should('include.text', 'Super')
   })
 })

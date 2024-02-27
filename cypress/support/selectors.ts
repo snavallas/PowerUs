1ded012be3ebafb95ca0fa16f28736ec39b363dd
Santiago J Navallas <snavallas@gmail.com>
2024-02-27 11:22:52 +0100

    Refactor code and add selectors

---
+++ cypress/support/selectors.ts
@@ -0,0 +1,18 @@
+const selectors = {
+    menuButton: `[aria-label='Menü öffnen']`,
+    menuCloseButton: `[aria-label='Menü schließen']`,
+    matButton: '.mat-button-wrapper',
+    matCard: '.mat-card-content',
+    cityInput: 'input[id="mat-input-1"]',
+    unternehmenInput: 'input[id="mat-input-2"]',
+    firstNameInput: 'input[id="mat-input-3"]',
+    lastNameInput: 'input[id="mat-input-4"]',
+    emailInput: 'input[id="mat-input-5"]',
+    phoneInput: 'input[id="mat-input-6"]',
+    chipText: '.chip-text',
+    funnelStep: 'app-funnel-steps',
+    locationStep: 'app-hiring-location-step',
+    contactStep: 'app-contact-information-step'
+}
+
+export { selectors }

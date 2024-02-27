9aba863a00fc3de55bcbe40ad791436ea1e76b79
Santiago J Navallas <snavallas@gmail.com>
2024-02-27 12:08:11 +0100

    Add README.md file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  // chromeWebSecurity: false,
-  // experimentalModifyObstructiveThirdPartyCode: true,
   e2e: {
     baseUrl: 'https://testing.powerus.de/',
   },
